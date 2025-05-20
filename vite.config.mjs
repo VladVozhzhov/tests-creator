f87af74c2c40fd04d95c9783a0bb02ce3a7622cc
Vlad Vozhzhov <vladvozhzhov@outlook.com>
2025-05-20 08:29:28 +0300

    Revert "made the website be ready for gh-pages", because gh pages don't support backend

--- vite.config.mjs
+++ vite.config.mjs
@@ -3,8 +3,7 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/tests-creator',
-  plugins: [react(), tailwindcss()],
+  plugins: [react(), tailwindcss(),],
   server: {
     proxy: {
       '/api': 'http://localhost:3500'
